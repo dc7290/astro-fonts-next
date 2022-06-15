75c931097435adad6c3c51805869c3fbf4ca504e
dc7290 <dhkh.cba0927@gmail.com>
2022-06-15 16:38:32 +0900

    feat: 🚀 Added font integration
    Added Astro integration that works like Next.js font optimization.

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import type { AstroIntegration } from 'astro'
+
+import plugin from './plugin'
+import { OPTIMIZED_FONT_PROVIDERS } from './utils/constants'
+
+const LIB_NAME = 'astro-fonts-next'
+
+export interface AstroFontsNextOptions {
+  url: string | string[]
+}
+
+export default (options: AstroFontsNextOptions): AstroIntegration => {
+  return {
+    name: LIB_NAME,
+    hooks: {
+      'astro:config:setup': async ({ command, updateConfig }) => {
+        if (!options.url || options.url === '' || options.url.length === 0) {
+          throw new Error(`[${LIB_NAME}]: you must set a \`url\` in your config!`)
+        }
+
+        const urls: string[] = []
+
+        if (typeof options.url === 'string') {
+          urls.push(options.url)
+        }
+        if (Array.isArray(options.url)) {
+          urls.push(...options.url)
+        }
+
+        // Check if the URL is valid
+        urls.forEach((url) => {
+          try {
+            new URL(url)
+          } catch (_) {
+            throw new Error(`[${LIB_NAME}]: \`${url}\` is not a valid URL.`)
+          }
+        })
+
+        urls.forEach((url) => {
+          if (OPTIMIZED_FONT_PROVIDERS.findIndex((providers) => url.startsWith(providers.url)) === -1) {
+            throw new Error(`[${LIB_NAME}]: \`${url}\` is not supported`)
+          }
+        })
+
+        updateConfig({
+          vite: { plugins: [plugin({ urls, command })] },
+        })
+      },
+    },
+  }
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const GOOGLE_FONT_PROVIDER = 'https://fonts.googleapis.com/'
+export const OPTIMIZED_FONT_PROVIDERS = [
+  { url: GOOGLE_FONT_PROVIDER, preconnect: 'https://fonts.gstatic.com' },
+  { url: 'https://use.typekit.net', preconnect: 'https://use.typekit.net' },
+]
