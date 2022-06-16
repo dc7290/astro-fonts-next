b8180bc4905ade911f605a0c15308ab81fb97936
dc7290 <dhkh.cba0927@gmail.com>
2022-06-16 18:56:19 +0900

    test: 💍 Added test directory when build-format is file

---
+++ __tests__/fixtures/multiple-file/astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config'
+
+import astroFonts from '../../../dist/index.js'
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    format: 'file',
+  },
+  integrations: [
+    astroFonts({
+      url: [
+        'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700',
+        'https://fonts.googleapis.com/css2?family=Lato:wght@400;700',
+      ],
+    }),
+  ],
+  experimental: {
+    integrations: true,
+  },
+})
