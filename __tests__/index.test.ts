a72486672eeda303b98e69db5040974f6763c3fd
dc7290 <dhkh.cba0927@gmail.com>
2022-06-17 14:38:39 +0900

    test: 💍 Added snapshot test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,7 +1,37 @@
-// import cheerio from 'cheerio'
-import { describe, test } from 'vitest'
+import { readFile } from 'fs/promises'
+import { globby } from 'globby'
+import { join } from 'path/posix'
+import { describe, expect, test } from 'vitest'
+
+const getDistDir = (fixturePath: string) =>
+  globby(join('__tests__/fixtures', fixturePath), {
+    expandDirectories: { extensions: ['html'] },
+  })
+const getHtmlData = async (fixturePath: string) => {
+  const distDir = await getDistDir(fixturePath)
+  return await Promise.all(
+    distDir.map(async (dir) => ({
+      path: dir,
+      content: await readFile(dir, 'utf-8'),
+    }))
+  )
+}
 
 describe('Build-time behavior', () => {
-  test.todo('Set a single value for url')
-  test.todo('Set multiple values for url')
+  test('Set a single value for url', async () => {
+    const htmlData = await getHtmlData('single')
+    expect(htmlData).toMatchSnapshot()
+  })
+  test('Set single values for url in file type', async () => {
+    const htmlData = await getHtmlData('single-file')
+    expect(htmlData).toMatchSnapshot()
+  })
+  test('Set multiple values for url', async () => {
+    const htmlData = await getHtmlData('multiple')
+    expect(htmlData).toMatchSnapshot()
+  })
+  test('Set multiple values for url in file type', async () => {
+    const htmlData = await getHtmlData('multiple-file')
+    expect(htmlData).toMatchSnapshot()
+  })
 })
